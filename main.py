64127cd8ac6f55336baa9dfebb8ad50d6e4cda2b
Brett Kaplan <darklink259@gmail.com>
2011-10-17 14:38:57 -0400

    More comments, more structure.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-
+import Queue
+import random
+from process import Process
 
 timer = 0
 
@@ -6,9 +8,38 @@ timerSwitch = 8
 
 running = true
 
+queue = Queue.Queue(0)
 
-
+#First-Come, First-Served (FCFS), with no preemption and no time slice
 while(running):
     
+    #output during simulation
     
+running = true
+#Shortest-Job-First (SJF), with no preemption and no time slice
+while(running):
+    
+    #output during simulation
+    
+running = true
+#Preemptive Shortest-Job-First (SJF), with preemption and no time slice
+while(running):
+    
+    #output during simulation
+    
+running = true
+#Round-Robin (RR), with configurable time slice t initially set to 100 milliseconds
+while(running):
+    
+    #output during simulation
 
+running = true
+#Preemptive Priority, with random priority levels 0-4 assigned to processes at the onset (low numbers indicate high priority); processes with the same priority are processed in FCFS order with no time slice; higher-priority processes entering the system interrupt and preempt a running process
+while(running):
+    
+    #output during simulation
+    
+#output for each scheduling algorithm:
+#Minimum, average, and maximum turnaround times
+#Minimum, average, and maximum initial wait times
+#Minimum, average, and maximum total wait times
